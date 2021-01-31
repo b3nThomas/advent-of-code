f029a806620cebfac3c65c3f76ee795e15a6972a
b3nThomas <b3nadthomas@gmail.com>
2021-01-31 23:28:04 +0000

    Types and started getNumberOfAdjacentOccupiedSeats method

--- src/days/11/run.ts
+++ src/days/11/run.ts
@@ -1,10 +1,27 @@
 import { getInput } from '../../utils/getInput';
 import { log } from '../../utils/log';
 
-const input = getInput(11)
+type OccupiedSeat = '#';
+type EmptySeat = 'L';
+type Floor = '.';
+
+type LayoutSectionState = OccupiedSeat | EmptySeat | Floor;
+type SeatLayout = LayoutSectionState[][];
+
+const input: SeatLayout = getInput(11)
     .split('\n')
-    .filter((row) => row);
+    .filter((row) => row)
+    .map((row) => row.split('')) as SeatLayout;
 
-log.info('11-1: %s', input);
+log.info({ input });
 
+const getNumberOfAdjacentOccupiedSeats: number = (currentSeatCoordinates: number[], layout: SeatLayout) => {
+    if (currentSeatCoordinates.length !== 2) {
+        throw new Error(
+            'currentSeat must be represented an array containing the x, y coordinates of the seat. Exiting'
+        );
+    }
+};
+
+// log.info('11-1: %s', input);
 // log.info('11-2: %s', <answer>);
