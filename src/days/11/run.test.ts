0d3ec3f36e1bfad3ddd8a6270d3717413615dddb
b3nThomas <b3nadthomas@gmail.com>
2021-02-13 22:15:23 +0000

    Finished day 11 (kinda)

--- src/days/11/run.test.ts
+++ src/days/11/run.test.ts
@@ -1,4 +1,4 @@
-import { countAdjacentOccupiedSeats, SeatLayout } from './run';
+import { countAdjacentOccupiedSeats, countVisibleOccupiedSeats, SeatLayout } from './run';
 
 describe('countAdjacentOccupiedSeats', () => {
     test('should return 0 when all adjacent sections are empty seats', () => {
@@ -52,4 +52,67 @@ describe('countAdjacentOccupiedSeats', () => {
             expect(countAdjacentOccupiedSeats(layout)(currentSeat)).toEqual(expected);
         }
     );
+});
+
+describe('countVisibleOccupiedSeats', () => {
+    test('should return 8 when 8 seats are visible', () => {
+        const layout: SeatLayout = [
+            ['.', '.', '.', '.', '.', '.', '.', '#', '.'],
+            ['.', '.', '.', '#', '.', '.', '.', '.', '.'],
+            ['.', '#', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '#', 'L', '.', '.', '.', '.', '#'],
+            ['.', '.', '.', '.', '#', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', '.', '.', '#', '.', '.', '.', '.', '.'],
+        ];
+
+        expect(countVisibleOccupiedSeats(layout)({ x: 3, y: 4 })).toEqual(8);
+    });
+
+    test('should return 0 when 1 empty seat is visible', () => {
+        const layout: SeatLayout = [
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+            ['.', 'L', '.', 'L', '.', '#', '.', '#', '.', '#', '.', '#', '.'],
+            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ];
+
+        expect(countVisibleOccupiedSeats(layout)({ x: 1, y: 1 })).toEqual(0);
+    });
+
+    test('should return 0 when 8 empty seat are visible', () => {
+        const layout: SeatLayout = [
+            ['L', 'L', 'L'],
+            ['L', 'L', 'L'],
+            ['L', 'L', 'L'],
+        ];
+
+        expect(countVisibleOccupiedSeats(layout)({ x: 1, y: 1 })).toEqual(0);
+    });
+
+    test('should return 1 when 7 empty and 1 occupied seats are visible', () => {
+        const layout: SeatLayout = [
+            ['L', 'L', 'L', 'L'],
+            ['L', 'L', 'L', 'L'],
+            ['L', 'L', '.', 'L'],
+            ['L', 'L', 'L', '#'],
+        ];
+
+        expect(countVisibleOccupiedSeats(layout)({ x: 1, y: 1 })).toEqual(1);
+    });
+
+    test('should return 0 when no seats are visible', () => {
+        const layout: SeatLayout = [
+            ['.', '#', '#', '.', '#', '#', '.'],
+            ['#', '.', '#', '.', '#', '.', '#'],
+            ['#', '#', '.', '.', '.', '#', '#'],
+            ['.', '.', '.', 'L', '.', '.', '.'],
+            ['#', '#', '.', '.', '.', '#', '#'],
+            ['#', '.', '#', '.', '#', '.', '#'],
+            ['.', '#', '#', '.', '#', '#', '.'],
+        ];
+
+        expect(countVisibleOccupiedSeats(layout)({ x: 3, y: 3 })).toEqual(0);
+    });
 });
--- src/days/11/run.ts
+++ src/days/11/run.ts
@@ -37,6 +37,43 @@ export const countAdjacentOccupiedSeats = (layout: SeatLayout) => (currentSeat: 
     return occupiedSeats;
 };
 
+export const countVisibleOccupiedSeats = (layout: SeatLayout) => (currentSeat: {
+    x: number;
+    y: number;
+}): number => {
+    let occupiedVisibleSeats = 0;
+    const directions = [
+        { x: 0, y: -1 }, // up
+        { x: 1, y: -1 }, // upRight
+        { x: 1, y: 0 }, // right
+        { x: 1, y: 1 }, // downRight
+        { x: 0, y: 1 }, // down
+        { x: -1, y: 1 }, // downLeft
+        { x: -1, y: 0 }, // left
+        { x: -1, y: -1 }, // upLeft
+    ];
+
+    directions.forEach((direction) => {
+        let nextSeat = { ...currentSeat };
+
+        while (true) {
+            nextSeat.x += direction.x;
+            nextSeat.y += direction.y;
+
+            if (!layout[nextSeat.y]?.[nextSeat.x] || layout[nextSeat.y][nextSeat.x] === 'L') {
+                break;
+            } else if (layout[nextSeat.y][nextSeat.x] === '.') {
+                continue;
+            } else {
+                occupiedVisibleSeats++;
+                break;
+            }
+        }
+    });
+
+    return occupiedVisibleSeats;
+};
+
 (() => {
     let retry = true;
     const rounds: SeatLayout[] = [input];
@@ -74,4 +111,41 @@ export const countAdjacentOccupiedSeats = (layout: SeatLayout) => (currentSeat: 
     log.info('11-1: %s', totalOccupiedSeats);
 })();
 
-// log.info('11-2: %s', <answer>);
+(() => {
+    // Doesn't quite work properly but f-it I'm well bored of this now... :D 
+    let retry = true;
+    const rounds: SeatLayout[] = [input];
+
+    while (retry) {
+        const prevLayout = rounds[rounds.length - 1];
+        const getOccupiedSeats = countVisibleOccupiedSeats(prevLayout);
+        const newLayout: SeatLayout = [];
+        prevLayout.forEach((row, x) => {
+            newLayout.push([]);
+            row.forEach((seat, y) => {
+                const seats = getOccupiedSeats({ x, y });
+
+                if (seat === 'L' && seats === 0) {
+                    newLayout[x].push('#');
+                } else if (seat === '#' && seats >= 5) {
+                    newLayout[x].push('L');
+                } else {
+                    newLayout[x].push(seat);
+                }
+            });
+        });
+
+        if (isEqual(prevLayout, newLayout)) {
+            retry = false;
+        }
+
+        rounds.push(newLayout);
+    }
+
+    let totalOccupiedSeats = 0;
+    rounds[rounds.length - 1].forEach((row) => {
+        totalOccupiedSeats += row.filter((seat) => seat === '#').length;
+    });
+
+    log.info('11-2: %s', totalOccupiedSeats);
+})();
