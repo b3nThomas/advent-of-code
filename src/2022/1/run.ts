37375bcfd7b68fbcece519fe92658d0402aad897
b3nThomas <b3nadthomas@gmail.com>
2022-12-04 23:55:18 +0000

    chore: generate day 1

---
+++ src/2022/1/run.ts
@@ -0,0 +1,7 @@
+import { getInput } from '../../utils/getInput';
+import { log } from '../../utils/log';
+
+const input = getInput(2022, 1).split('\n');
+
+log.info('1-1: %s', '<answer>');
+log.info('1-2: %s', '<answer>');
