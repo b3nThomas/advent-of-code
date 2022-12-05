ce929bcecb65f36f2181248ad63ed92cf1f913f0
b3nThomas <b3nadthomas@gmail.com>
2022-12-05 01:19:43 +0000

    chore: generate template / add input

---
+++ src/2022/2/run.ts
@@ -0,0 +1,7 @@
+import { getInput } from '../../utils/getInput';
+import { log } from '../../utils/log';
+
+const input = getInput(2022, 2).split('\n');
+
+log.info('2-1: %s', '<answer>');
+log.info('2-2: %s', '<answer>');
