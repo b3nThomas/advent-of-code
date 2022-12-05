bddfa67844a32774bd06dbca698d86b3f96529a2
b3nThomas <b3nadthomas@gmail.com>
2022-12-05 14:05:59 +0000

    chore: tweak day 1

--- src/2022/1/run.ts
+++ src/2022/1/run.ts
@@ -1,12 +1,12 @@
 import { getInput } from '../../utils/getInput';
 import { log } from '../../utils/log';
-import collectCalories from './collectCaloriesTotals';
+import { collectCaloriesTotals } from './collectCaloriesTotals';
 import { getHighest, getHighestThree } from './getHighestCalories';
 
 const input = getInput(2022, 1).split('\n');
 
-const answer1 = getHighest(collectCalories(input));
-const answer2 = getHighestThree(collectCalories(input));
+const answer1 = getHighest(collectCaloriesTotals(input));
+const answer2 = getHighestThree(collectCaloriesTotals(input));
 
 log.info('1-1: %s', answer1);
 log.info('1-2: %s', answer2);
--- src/2022/1/types.ts
+++ src/2022/1/types.ts
@@ -1,6 +1,3 @@
 type TotalCalories = number;
-/**
- * Use tuple so we can easily sort on TotalCalories
- * and extract the Elf with the highest TotalCalories
- */
+
 export type CaloriesTotals = TotalCalories[];
