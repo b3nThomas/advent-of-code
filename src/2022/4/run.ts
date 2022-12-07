8d47b32923665aefb376e63bc9428f882e91c60b
b3nThomas <b3nadthomas@gmail.com>
2022-12-07 21:44:37 +0000

    chore: generate template

---
+++ src/2022/4/run.ts
@@ -0,0 +1,10 @@
+import { log } from '../../utils/log';
+import { getInput } from '../../utils/getInput';
+
+const input = getInput(2022, 4).split('\n');
+
+const answer1 = 'TODO';
+const answer2 = 'TODO';
+
+log.info('4-1: %s', answer1);
+log.info('4-2: %s', answer2);
