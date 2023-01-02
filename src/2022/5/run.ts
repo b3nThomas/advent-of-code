6f6b0a64546c90109d3bae5e0fa1906a622387d4
Ben Thomas <b3nadthomas@gmail.com>
2023-01-02 01:09:31 +0000

    chore: scaffold day-5-22

---
+++ src/2022/5/run.ts
@@ -0,0 +1,9 @@
+import { getInput, log } from '../../lib';
+
+const input = getInput(2022, 5).split('\n');
+
+const answer1 = 'TODO';
+const answer2 = 'TODO';
+
+log.info('5-1: %s', answer1);
+log.info('5-2: %s', answer2);
