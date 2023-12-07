585d52544ec0ff637dbf71f318981b37632d690d
b3nThomas <b3nadthomas@gmail.com>
2023-12-07 23:36:46 +0000

    feat: day 2 part 2

--- src/2023/2/run.ts
+++ src/2023/2/run.ts
@@ -4,7 +4,7 @@ const input = getInput(2023, 2).split('\n');
 
 type CubeColor = 'red' | 'blue' | 'green';
 
-const highestValueColor = (color: CubeColor, text: string): number => {
+const maxValueForColor = (color: CubeColor, text: string): number => {
     const pattern = new RegExp(`(\\d+)\\s+${color}`, 'g');
     const matches = text.match(pattern);
 
@@ -13,16 +13,22 @@ const highestValueColor = (color: CubeColor, text: string): number => {
 
 const games = input.map((line, i) => ({
     id: i + 1,
-    red: highestValueColor('red', line),
-    blue: highestValueColor('blue', line),
-    green: highestValueColor('green', line),
+    red: maxValueForColor('red', line),
+    blue: maxValueForColor('blue', line),
+    green: maxValueForColor('green', line),
 }));
 
-const validGames = games.filter(({ red, blue, green }) => red <= 12 && blue <=14 && green <= 13);
-const validGameIdsTotal = validGames.reduce((acc, cur) => acc += cur.id, 0);
+const validGames = games.filter(
+    ({ red: red, blue: blue, green: green }) => red <= 12 && blue <= 14 && green <= 13
+);
+const validGameIdsTotal = validGames.reduce((acc, cur) => (acc += cur.id), 0);
 
 const answer1 = validGameIdsTotal;
-const answer2 = 'TODO';
+
+const minValuesPowered = games.map(({ red, blue, green }) => red * blue * green);
+const powersTotalled = minValuesPowered.reduce((acc, cur) => (acc += cur), 0);
+
+const answer2 = powersTotalled;
 
 log.info('2-1: %s', answer1);
 log.info('2-2: %s', answer2);
